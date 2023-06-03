f8b9ef8e6a27331a8c12781ff3cab6afa52c2325
Sciencekill <sciencekill@outlook.com>
2023-06-03 11:28:08 +0800

    sks-toolkit normal update

---
+++ BindingData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sks_toolkit
+{
+    public class BindingData
+    {
+        private string currentuser;
+        public string CurrentUser
+        {
+            set { this.currentuser = value; }
+            get { return this.currentuser; }
+        }
+        private string greetingword;
+        public string GreetingWord
+        {
+            set { this.greetingword = value; }
+            get { return this.greetingword; }
+        }
+    }
+}
--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -10,12 +10,40 @@ namespace sks_toolkit.Views
             InitializeComponent();
             Init();
         }
-        public void Init()
+        public void Init()//初始化变量
         {
-            Data data = new Data();
-            data.CurrentUser = System.Environment.UserName;
-            data.GreetingWord= "aaa";
-            MainTab.DataContext = data;
+            BindingData data = new BindingData();//创建binding类
+            data.CurrentUser = System.Environment.UserName;//获取用户名
+            int TimeNow = System.DateTime.Now.Hour;
+            if (TimeNow >= 1 && TimeNow < 5)
+            {
+                data.GreetingWord = "凌晨好,";
+            }
+            else if (TimeNow >= 5 && TimeNow < 8)
+            {
+                data.GreetingWord = "早上好,";
+            }
+            else if(TimeNow >= 8 && TimeNow < 11)
+            {
+                data.GreetingWord = "上午好,";
+            }
+            else if(TimeNow>=11 && TimeNow < 13)
+            {
+                data.GreetingWord = "中午好,";
+            }
+            else if(TimeNow>=13 && TimeNow < 17)
+            {
+                data.GreetingWord = "下午好,";
+            }
+            else if(TimeNow>=17 && TimeNow < 19)
+            {
+                data.GreetingWord = "傍晚好";
+            }
+            else
+            {
+                data.GreetingWord = "晚上好";
+            }
+            MainTab.DataContext = data;//设置绑定源
         }
     }
 }
