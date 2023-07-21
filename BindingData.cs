82e7510037055bbe55051669729e991a3fc20142
添宇 程 <sciencekill@outlook.com>
2023-07-21 18:32:32 +0800

    Sciencekill's Toolkit Everyday Update

---
+++ BindingData.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+
+namespace sks_toolkit
+{
+    internal class BindingData
+    {
+        private string currentuser = "ERROR?";
+        public string CurrentUser
+        {
+            set { currentuser = value; }
+            get { return currentuser; }
+        }
+        private string greetingword = "ERROR?";
+        public string GreetingWord
+        {
+            set { greetingword = value; }
+            get { return greetingword; }
+        }
+        private string greetingsentence = "ERROR?";
+        public string GreetingSentence
+        {
+            set { greetingsentence = value; }
+            get { return greetingsentence; }
+        }
+        private string version = "ERROR?";
+        public string Version
+        {
+            set { version = value; }
+            get { return version; }
+        }
+        private string channel = "ERROR?";
+        public string Channel
+        {
+            set { channel = value; }
+            get { return channel; }
+        }
+        private string build = "ERROR?";
+        public string Build
+        {
+            set { build = value; }
+            get { return build; }
+        }
+        private string latestversion = "ERROR?";
+        public string latestVersion
+        {
+            set { latestversion = value; }
+            get { return latestversion; }
+        }
+        private string latestornot = "ERROR?";
+        public string latestOrNot
+        {
+            set { latestornot = value; }
+            get { return latestornot; }
+        }
+    }
+    internal class Deploy_ENV_Data
+    {
+        private JObject download_link = new JObject();
+        public JObject Download_Link
+        {
+            set { download_link = value; }
+            get { return download_link; }
+        }
+        private List<string> gpp_version_list = new List<string>();
+        public List<string> Gpp_version_list
+        {
+            get { return gpp_version_list; }
+            set { gpp_version_list = value; }
+        }
+        private string install_gpp = "true";
+        public string Install_gpp
+        {
+            set { install_gpp = value; }
+            get { return install_gpp; }
+        }
+        private string install_gpp_version = "8.1.0-seh";
+        public string Install_gpp_version
+        {
+            set { install_gpp_version = value; }
+            get { return install_gpp_version; }
+        }
+        private string install_path = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+        public string Install_path
+        {
+            set { 
+                install_path = value;
+            }
+            get { return install_path;}
+        }
+        public void StartDeployClicked()
+        {
+
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace sks_toolkit
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BindingData data = new BindingData();//创建binding类
+        Deploy_ENV_Data deploy_env_data = new Deploy_ENV_Data();
+        public MainWindow()
+        {
+            InitializeComponent();
+            Init();
+        }
+        public void Init()
+        {
+            data.CurrentUser = System.Environment.UserName;//获取用户名
+            int TimeNow = System.DateTime.Now.Hour;//获取时间
+            //根据时间指定问候
+            if (TimeNow >= 1 && TimeNow < 5)
+            {
+                data.GreetingWord = "凌晨好,";
+                data.GreetingSentence = "怎么起床这么早?";
+            }
+            else if (TimeNow >= 5 && TimeNow < 8)
+            {
+                data.GreetingWord = "早上好,";
+                data.GreetingSentence = "美好的一天又开始了!";
+            }
+            else if (TimeNow >= 8 && TimeNow < 11)
+            {
+                data.GreetingWord = "上午好,";
+                data.GreetingSentence = "忙碌的一天~";
+            }
+            else if (TimeNow >= 11 && TimeNow < 13)
+            {
+                data.GreetingWord = "中午好,";
+                data.GreetingSentence = "可以休息一下了~";
+            }
+            else if (TimeNow >= 13 && TimeNow < 17)
+            {
+                data.GreetingWord = "下午好,";
+                data.GreetingSentence = "精神不好就去喝杯咖啡吧!";
+            }
+            else if (TimeNow >= 17 && TimeNow < 19)
+            {
+                data.GreetingWord = "傍晚好,";
+                data.GreetingSentence = "累了就看看窗外，休息一下~";
+            }
+            else
+            {
+                data.GreetingWord = "晚上好,";
+                data.GreetingSentence = "你在熬夜吗?";
+            }
+            //读取版本号
+            string workDictionary = System.AppDomain.CurrentDomain.BaseDirectory;
+            string configPath = workDictionary + "Assets\\\\Config.json";
+            JObject Config;
+            using (System.IO.StreamReader configFile = System.IO.File.OpenText(configPath))
+            {
+                using (JsonTextReader jsonReader = new JsonTextReader(configFile))
+                {
+                    Config = (JObject)JToken.ReadFrom(jsonReader);
+                }
+            }
+            data.Version = Config["version"].ToString();
+            data.Channel = Config["channel"].ToString();
+            data.Build = Config["build"].ToString();
+            //获取最新版本
+            JObject LatestRelease = WebService.getJsonFromServer("https://gitee.com/sciencekiller/sks-toolkit/raw/main/Assets/Config.json");
+            if (LatestRelease != null)
+            {
+                data.latestVersion = LatestRelease["version"].ToString();
+            }
+            else
+            {
+                data.latestVersion = "NetWorkConnectError";
+                data.latestOrNot = "连接到Gitee失败，请开启代理";
+            }
+            if (data.Version == data.latestVersion)
+            {
+                data.latestOrNot = "你正在使用最新的Sciencekill's Toolkit";
+            }
+            else if (data.Version != data.latestVersion && LatestRelease != null)
+            {
+                data.latestOrNot = "有新版本可用，请尽快查看";
+            }
+            List<string> gpp_version_list = new List<string>();
+            string latest_gpp_version_list_url = "https://gitee.com/sciencekiller/sks-toolkit/raw/main/Assets/Gpp_Download_List.json";
+            JObject latest_gpp_version_list = WebService.getJsonFromServer(latest_gpp_version_list_url);
+            foreach (var gpp_version in latest_gpp_version_list)
+            {
+                gpp_version_list.Add(gpp_version.Key);
+            }
+            deploy_env_data.Gpp_version_list = gpp_version_list;
+            deploy_env_data.Download_Link = latest_gpp_version_list;
+            //设置绑定源
+            MainTab.DataContext = data;
+            DeployEnvTab.DataContext = deploy_env_data;
+        }
+    }
+}
