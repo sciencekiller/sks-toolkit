0a8b9cb8c33b4ebffed4c4eef14043c14e014954
sciencekill <sciencekill@outlook.com>
2023-09-17 20:22:03 +0800

    update

--- FirstLaunch/MainFrame.xaml.cs
+++ FirstLaunch/MainFrame.xaml.cs
@@ -2,6 +2,7 @@ using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.Win32;
+using System.Diagnostics;
 using System.Text.Json.Nodes;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -13,44 +14,12 @@ namespace sks_toolkit.FirstLaunch
     /// </summary>
     public sealed partial class MainFrame : Page
     {
-        private bool currentColorMode;
         private int currentSet = 0;
         private JsonObject settings=new JsonObject();
         public MainFrame()
         {
             InitializeComponent();
-            currentColorMode = Scripts.ThemeHelper.isUsingDarkMode();
-            SetTheme();
-            SystemEvents.UserPreferenceChanged += new UserPreferenceChangedEventHandler(OnThemeChanged);
-            settingFrame.Navigate(typeof(Pages.SetLanguage));
-        }
-        private void OnThemeChanged(object sender, UserPreferenceChangedEventArgs e)
-        {
-            if (currentColorMode != Scripts.ThemeHelper.isUsingDarkMode()) currentColorMode = !currentColorMode;
-            else return;
-            if (currentColorMode) SetThemeToDark();
-            else SetThemeToLight();
-            return;
-        }
-        private void SetTheme()
-        {
-            if (currentColorMode) SetThemeToDark();
-            else SetThemeToLight();
-            return;
-        }
-        private void SetThemeToDark()
-        {
-            AcrylicBrush settingBoxBrush = (AcrylicBrush)Application.Current.Resources["FirstLaunchSettingBoxBrushDark"];
-            SettingBox.Background = settingBoxBrush;
-            SolidColorBrush nextButtonBrush = (SolidColorBrush)Application.Current.Resources["FirstLaunchNextButtonBrushDark"];
-            NextStepButton.Background = nextButtonBrush;
-        }
-        private void SetThemeToLight()
-        {
-            AcrylicBrush settingBoxBrush = (AcrylicBrush)Application.Current.Resources["FirstLaunchSettingBoxBrushLight"];
-            SettingBox.Background = settingBoxBrush;
-            SolidColorBrush nextButtonBrush = (SolidColorBrush)Application.Current.Resources["FirstLaunchNextButtonBrushLight"];
-            NextStepButton.Background = nextButtonBrush;
+            SettingFrame.Navigate(typeof(Pages.SetLanguage));
         }
         private void NextStepButton_Click(object sender, RoutedEventArgs e)
         {
